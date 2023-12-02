490696806460eda9518accf9b42bcb4c8091d28d
douranos <Nikos.Douranos@ibm.com>
2023-12-02 21:05:21 +0200

    1st version of Questions [FIXED]

--- src/main/java/com/quiz/demoquiz/Quiz.java
+++ src/main/java/com/quiz/demoquiz/Quiz.java
@@ -1,18 +1,50 @@
 package com.quiz.demoquiz;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Column;
+import javax.persistence.Table;
 
-@Data
 @Entity
+@Table(name = "travel_questions")
 public class Quiz {
-
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
-    private Integer QuizIndex;
-    private String QuestionTitle;
-    private String QOption1;
-    private String QOption2;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length = 255)
+    private String category;
+
+    @Column(columnDefinition = "TEXT")
+    private String question;
+
+    // Getter and setter for id
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    // Getter and setter for category
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    // Getter and setter for question
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
 }
+
--- src/main/java/com/quiz/demoquiz/controller/ControllerApp.java
+++ src/main/java/com/quiz/demoquiz/controller/ControllerApp.java
@@ -1,23 +1,36 @@
 package com.quiz.demoquiz.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.quiz.demoquiz.Quiz;
 import com.quiz.demoquiz.service.QuestionService;
 
+import org.springframework.ui.Model;
+
+
 import java.util.List;
 
-@RestController
-@RequestMapping("quiz")
+@Controller
+@RequestMapping("/quiz")
 public class ControllerApp {
+
     @Autowired
     QuestionService questionService;
-    @GetMapping("questions")
-    public List<Quiz> getAllQuestions(){
-        return questionService.getAllQuestions();
+
+    @GetMapping("/questions/{id}")
+    public String getQuestion(Model model, @PathVariable int id) {
+        List<Quiz> allQuestions = questionService.getAllQuestions();
+        if (id >= 0 && id < allQuestions.size()) {
+            model.addAttribute("question", allQuestions.get(id));
+            model.addAttribute("currentIndex", id);
+            model.addAttribute("totalQuestions", allQuestions.size());
+        }
+        return "questions"; // Name of the Thymeleaf template
     }
-    
 }
+
